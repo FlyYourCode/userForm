2ec7d899f06f4d1573d109625097e836b41fed80
fengxiaoming <984307510@qq.com>
2021-01-19 16:32:49 +0800

    test11

--- app/src/main/java/com/sport/from/MainActivity.java
+++ app/src/main/java/com/sport/from/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<String> strings) {
                 validateV6(strings);
+
+
             }
         });
     }
